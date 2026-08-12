191d7400917d696875103a2b3a76c9f5d0625f06
zerotocodeeklas <eklasalsiyabi08@gmail.com>
2026-08-12 19:58:18 +0400

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,34 @@
+name = "Eklas"
+print(name)
+
+price=10.1
+print(price)
+
+name="Noor"
+print(type(name))
+
+num=100
+print(type(num))
+
+pri=10.12
+print(type(pri))
+
+student=True
+print(type(student))
+
+
+age=int(input("Enter you age"))
+print(age+10)
+
+
+number1=int(input("Enter the first number :"))
+number2=int(input("Enter the sceond number :"))
+print(number1+number2)
+
+pay=float(input("Enter the price :"))
+print(pay + 5)
+
+
+
+
+
---
+++ day2.py
@@ -0,0 +1,20 @@
+number1=int(input("Enter first number : "))
+number2=int(input("Enter second number :"))
+print("The result :",number1+number2)
+print("The result :",number1-number2)
+print("The result :",number1*number2)
+print("The result :",number1/number2)
+
+height=int(input("Enter the height : "))
+width=int(input("Enter the width : "))
+print("The total area = ", height*width)
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+total = num1 + num2 + num3
+average = total / 3
+
+print("Total =", total)
+print("Average =", average)
