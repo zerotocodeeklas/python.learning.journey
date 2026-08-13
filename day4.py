dcdacf0ea5e4b40789fbc420bb5aecab2edebb68
zerotocodeeklas <eklasalsiyabi08@gmail.com>
2026-08-13 20:28:42 +0400

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,34 @@
+age=23
+if age>= 18:
+    print("You are an adult")
+else:
+    print("you are under 18")
+
+
+password=123456
+if password==123457:
+    print("Welcome!")
+else:
+    print("Wrong password")
+
+
+Age=int(input("Enter your age:"))
+if Age>=18:
+    print("You are allowed")
+else:
+    print("You are not allowed")
+
+
+Pass=input("Enter your password : ")
+if Pass=="python123":
+    print("Login successful")
+else:
+    print("Wrong password")
+
+number=int(input("Enter a number: "))
+if number>0:
+    print("positive")
+elif number<0:
+    print("Negatine")
+else:
+    print("zero")
