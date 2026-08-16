4cfe4e382c688b7a89c49543df155fff9f95193f
zerotocodeeklas <eklasalsiyabi08@gmail.com>
2026-08-16 19:43:09 +0400

    Add files via upload
    Add Boolean variables and logical conditions practice

---
+++ day7.py
@@ -0,0 +1,47 @@
+#Create three Boolean variables:
+#is_student
+#is_graduated
+#has_job
+#Give them appropriate True or False values and print them.
+
+is_student = False
+is_graduated = True
+has_job = True
+
+print(is_student)
+print(is_graduated)
+print(has_job)
+
+
+
+#Create a Boolean variable called is_python_fun and print its value and its data type.
+is_python_fun=True
+print(is_python_fun)
+print(type(is_python_fun))
+
+
+
+#Create a Boolean variable called is_logged_in.
+#If the value is True, print "Welcome!".
+#Otherwise, print "Please login".
+is_logged_in = True
+if is_logged_in:
+    print("Welcome!")
+else:
+    print("Please login")
+
+
+
+#Create two Boolean variables:
+#has_username
+#has_password
+#If the user has both a username AND a password, print "Login successful".
+#Otherwise, print "Please enter your username and password".
+
+has_username=True
+has_password=True
+if has_username and has_password:
+    print("Login Sucessful")
+else:
+    print("plase enter yser name and password")
+
