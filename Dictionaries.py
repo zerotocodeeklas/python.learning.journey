5ec87d669adbdd6b566a9a0c42f8c52d6d89ead2
zerotocodeeklas <eklasalsiyabi08@gmail.com>
2026-09-01 19:35:41 +0400

    Add files via upload
    Start Python dictionaries practice

---
+++ Dictionaries.py
@@ -0,0 +1,33 @@
+#Create a dictionary called student.Add the following information:name,age,major,Then print the dictionary.
+Student={
+    "name":"Noor",
+    "age":23,
+    "major": "Computer Science"
+}
+print(Student)
+
+
+#Create a dictionary containing your name, age, and city.Print your name separately using its key.Print your age separately using its key.
+
+My_Information={
+    "Name":"Eklas",
+    "Age":24,
+    "city":"Muscat"
+}
+print(My_Information["name"])
+print(My_Information["Age"])
+
+#Create a dictionary called student containing a student's name and age.Change the student's age.Then print the dictionary.
+
+Student = {
+    "Name": "Kawther",
+    "age": "25"
+}
+
+Student["age"] = 23
+
+print(Student)
+
+
+
+
