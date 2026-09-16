0b65ef87318dc0b69d5a390f133905e109148a5c
zerotocodeeklas <eklasalsiyabi08@gmail.com>
2026-09-16 17:21:42 +0400

    Add files via upload
    Parameters & function

---
+++ function.py
@@ -0,0 +1,11 @@
+#Create a function called welcome() that prints "Welcome to Python!" and call the function.
+def welcome():
+    print("Welcome to Python!")
+welcome()
+
+#Create a function called greet(name).The function should print:
+def greet(name):
+    print("Hello ", name)
+greet("Eklas")
+greet("Kawther")
+greet("Noor")
