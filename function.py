b704842b7e27a97c68d856701f5f1528a737cc69
zerotocodeeklas <eklasalsiyabi08@gmail.com>
2026-09-19 13:44:01 +0400

    Add student_info function to display name and age
    Added a new function student_info() to print student's name and age.

--- function.py
+++ function.py
@@ -9,3 +9,16 @@ def greet(name):
 greet("Eklas")
 greet("Kawther")
 greet("Noor")
+
+
+#Create a function called greet(name).The function should print:
+def greet(name):
+    print("Hello ", name)
+greet("Eklas")
+
+
+#Create a function called student_info(name, age) that prints the student's name and age.
+def student_info(name, age):
+    print("Student Name: ", name)
+    print("Student Age: ", age)
+student_info("Eklas", 24)
