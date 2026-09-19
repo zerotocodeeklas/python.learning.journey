976a38d1eb35c09a8086717f89f9c8a60177638d
zerotocodeeklas <eklasalsiyabi08@gmail.com>
2026-09-19 14:04:35 +0400

    Return + Calculation

---
+++ return.py
@@ -0,0 +1,36 @@
+#Return
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print(result)
+
+
+
+def Multiply(x, y):
+    return x * y
+
+result = Multiply(5, 3)
+print(result)
+
+
+def add_num(a,b):
+    return a + b
+
+result = add_num(10,5)
+print(result)
+
+
+def calculate_average(a,b,c):
+    return(a+b+c)/3
+result = calculate_average(10,20,30)
+print(result)
+
+
+#Return + Calculation
+def calculate_total(price, quantity):
+    total = price * quantity
+    return total
+
+result = calculate_total(10, 5)
+print(result)
